dcbddeb766c4edf122bd44af3386a393b110ecbc
realnumber666 <781504900@qq.com>
2019-03-30 20:24:18 +0800

    通过日期和第几节课进行查询

--- main.py
+++ main.py
@@ -2,19 +2,16 @@
 import requests
 import re
 import json
+import sys
 from bs4 import BeautifulSoup
-
-url = "http://hub.m.hust.edu.cn/aam/room/selectFreeRoom.action?buildingCode=%27D120,%E4%B8%9C%E5%8D%81%E4%BA%8C%E6%A5%BC%27&borrowDate=%272019-03-30%27&section=%279-10%E8%8A%82%27"
-
+# 参数格式：第一个为日期2019-03-30，第二个为第几节1-2
+url = "http://hub.m.hust.edu.cn/aam/room/selectFreeRoom.action?buildingCode=%27D120,%E4%B8%9C%E5%8D%81%E4%BA%8C%E6%A5%BC%27&borrowDate=%27" + sys.argv[1] + "%27&section=%27" + sys.argv[2] + "%E8%8A%82%27"
+print(url)
 headers = {'Cookie': 'JSESSIONID=0000J2XFamfH9Aqe6oR0g0xW4lZ:-1'}
 r = requests.get(url, headers = headers)
 soup = BeautifulSoup(r.content)
 i = 0
 pattern = re.compile("var json='(.*?)';")
-# for child in soup.html.contents:
-#     i+=1
-#     print('--------'+str(i))
-#     print(child)
 jsScript = soup.html.contents[5].string
 jsScript = jsScript.strip().replace('\n','')
 m = pattern.match(jsScript).groups()[0]
